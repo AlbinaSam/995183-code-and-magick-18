fec7b52daca5db4038e7cbc94513beec0843da81
AlbinaSam <alba91@inbox/ru>
2019-10-07 12:56:41 +0300

    Модули и перемещение

---
+++ js/util.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+
+  window.util = {
+    getRandomElement: function (elements) {
+      var randomValue = elements[Math.floor(Math.random(elements) * elements.length)];
+      return randomValue;
+    },
+
+    ESC_KEYCODE: 27,
+
+    ENTER_KEYCODE: 13,
+
+    setup: document.querySelector('.setup')
+
+  };
+
+})();
